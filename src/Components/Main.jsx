10273a5bd14e025ffae118ec69dab6b36363e5f0
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-12-13 14:57:32 +0100

    create main component

---
+++ src/Components/Main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <>
+      <section>
+        <div>
+          <h1>Designed for the future</h1>
+        </div>
+        <div>
+          <div>
+            <div>
+              <h2>Introducing an extensible editor</h2>
+              <p>Blogr features are extremely intuitive interface which let you to focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy way to add functionality or change the look of a blog </p>
+            </div>
+            <div>
+              <h2>Robust content management</h2>
+              <p>Flexible content management enables users to easily move through posts, increase the usability of your blog by adding customized categories, sections, format, or flow. with this functionality, you're in full control. </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Main
