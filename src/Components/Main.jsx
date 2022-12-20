af1af4af61040dfab1a9646ba3589e4b0d95f53b
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-12-20 19:05:25 +0100

    Add responsive styles to the second and first section

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import illustration from '../images/illustration-editor-desktop.svg';
+import phone from '../images/illustration-phones.svg'
+import patternCircle from '../images/bg-pattern-circles.svg'
 
 function Main() {
   return (
     <>
       <section className='relative overflow-hidden box-border mb-20'>
-        <div className='container mx-auto box-border py-32 text-[#3F4164]'>
+        <div className='container mx-auto box-border mmd:py-32 text-[#3F4164]'>
           <div className='bg-transparent  box-border'>
             <h1 className='text-center text-3xl bg-transparent'>Designed for the future</h1>
           </div>
           <div className='flex flex-col-reverse mmd:flex-row border-box'>
-            <div className='flex-1 border-box'>
-              <div className='px-24 py-20'>
+            <div className='flex-1 text-center mmd:text-left border-box'>
+              <div className='px-16  mmd:px-24 py-20'>
                 <h2 className='text-xl pb-4 font-medium'>Introducing an extensible editor</h2>
                 <p>Blogr features are extremely intuitive interface which let you to focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy way to add functionality or change the look of a blog </p>
               </div>
-              <div className='px-24'>
+              <div className='px-16 mmd:px-24'>
                 <h2 className='text-xl pb-4 font-medium'>Robust content management</h2>
                 <p >Flexible content management enables users to easily move through posts, increase the usability of your blog by adding customized categories, sections, format, or flow. with this functionality, you're in full control. </p>
               </div>
@@ -29,9 +31,24 @@ function Main() {
           </div>
         </div>
       </section>
-      <section className='bg-[#3F4164]'>
-      
-        this is me
+      <section className='bg-[#3A3B59] mt-52 mmd:mt-4 relative rounded-tr-[8rem] rounded-bl-[8rem]'>
+        <img className='absolute z-10 -top-[10rem] mmd:-top-[4.7rem] mmd:left-12' src={phone} alt="phone" />
+        <div className='mmd:flex mmd:gap-12 relative overflow-hidden'>
+          <div className='flex-1'>
+            <img className='relative mmd:absolute -top-[10rem] -left-[5rem] mmd:-top-[35rem] mmd:-left-[13rem]' src={patternCircle} alt="patternCircle" />
+          </div>
+          <div className='flex-1 relative px-16 text-center  mmd:px-28 py-32 text-white'>
+            <div className='absolute mmd:relative -top-[10rem] mmd:top-0 px-20'>
+              <h1 className='text-2xl font-medium pb-8'>State of Art Infrastructure</h1>
+              <p >With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section >
+        <div className='container mx-auto'>
+
+        </div>
       </section>
     </>
   )
